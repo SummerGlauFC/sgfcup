f624e77f304cdd3d53e73490647f1fb9df593faf
Remilia-Scarlet <russel@sgfc.co>
2015-01-02 23:47:47 +0800

    Add example config file.

---
+++ project/config.example.py
@@ -0,0 +1,58 @@
+from db import DB
+import functions
+from picklesettings import PickleSettings
+
+base = "/path/to/base/folder"  # base folder
+
+Settings = {
+    "ssl": True, # if to use https in urls or not
+    "directories": {
+        "files": base + "/img/p/",  # private image location
+        "thumbs": base + "/img/t/",  # thumbnail location
+        "url": "sgfc.co"  # homeurl
+    },
+    "database": {
+        "user": "username",  # database user
+        "password": "password",  # db user password
+        "db": "database_name"  # database name
+    },
+    "admin": {
+        "username": "password" # admin login details
+    }
+}
+
+Settings["directories"]["base"] = base
+
+# /-----------------------------------------------------------------\
+# |  Please try not to adjust any of the below without good reason. |
+# \-----------------------------------------------------------------/
+
+# Sortmodes used by the gallery ("Name", "database column", "sort order")
+sortmodes = [
+    ["Date (descending)", "id", "DESC"],
+    ["Date (ascending)", "id", "ASC"],
+    ["Hits (descending)", "hits", "DESC"],
+    ["Hits (ascending)", "hits", "ASC"]
+]
+
+# Search modes used by the gallery ("Name", "database column")
+searchmodes = [
+    ["Filename", "original"],
+    ["Short URL", "shorturl"]
+]
+
+# File type definitions used by the gallery
+file_types = ["image", "file", "paste"]
+file_type = {
+    "image": 0,
+    "file": 1,
+    "paste": 2
+}
+
+# Initalize database connection
+db = DB(user=Settings['database']['user'], password=Settings['database']
+        ['password'], database=Settings['database']['db'])
+
+# This json file contains the configuration for all user settings.
+# Default settings can be changed in this file.
+user_settings = PickleSettings("project/user_settings.json", db)
