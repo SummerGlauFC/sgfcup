f7331d31eb5432a0fcb8458a748fa002866d179b
Remilia-Scarlet <russel@sgfc.co>
2015-05-09 23:44:12 +0800

    Add new constants file.

--- project/config.example.py
+++ project/config.example.py
@@ -1,6 +1,5 @@
-from db import DB
 import functions
-from picklesettings import PickleSettings
+from configdefines import *
 
 base = "/path/to/base/folder"  # base folder
 
@@ -22,35 +21,6 @@ Settings = {
     "admin": {
         "username": "password" # admin login details
     }
-}
-
-Settings["directories"]["base"] = base
-
-# /-----------------------------------------------------------------\
-# |  Please try not to adjust any of the below without good reason. |
-# \-----------------------------------------------------------------/
-
-# Sortmodes used by the gallery ("Name", "database column", "sort order")
-sortmodes = [
-    ["Date (descending)", "id", "DESC"],
-    ["Date (ascending)", "id", "ASC"],
-    ["Hits (descending)", "hits", "DESC"],
-    ["Hits (ascending)", "hits", "ASC"],
-    ["Size", "size", "DESC"]
-]
-
-# Search modes used by the gallery ("Name", "database column")
-searchmodes = [
-    ["Filename", "original"],
-    ["Short URL", "shorturl"]
-]
-
-# File type definitions used by the gallery
-file_types = ["image", "file", "paste"]
-file_type = {
-    "image": 0,
-    "file": 1,
-    "paste": 2
 }
 
 # Initalize database connection
